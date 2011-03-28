8f865d3681b380126e9c915f23cea658191836f6
DelphiDabbler <git@delphidabbler.com>
2011-03-28 15:45:36 +0000

    vis: * Removed .dof.tplt files: not used by Delphi 2010. * Modified Config.bat so is doesn't try to create .dof files from .dof.tplt files.

--- DevTools/Config.bat
+++ DevTools/Config.bat
@@ -39,28 +39,23 @@ echo .
 
 setlocal
 
-echo Creating .cfg and .dof files from templates:
+echo Creating .cfg files from templates:
 cd ..\Src
 
 echo   FileVer
 copy App\FileVer.cfg.tplt App\FileVer.cfg
-copy App\FileVer.dof.tplt App\FileVer.dof
 
 echo   FileVerCmd
 copy CmdApp\FileVerCmd.cfg.tplt CmdApp\FileVerCmd.cfg
-copy CmdApp\FileVerCmd.dof.tplt CmdApp\FileVerCmd.dof
 
 echo   FileVerCM
 copy CtxMenu\FileVerCM.cfg.tplt CtxMenu\FileVerCM.cfg
-copy CtxMenu\FileVerCM.dof.tplt CtxMenu\FileVerCM.dof
 
 echo   FVFileReader
 copy Reader\FVFileReader.cfg.tplt Reader\FVFileReader.cfg
-copy Reader\FVFileReader.dof.tplt Reader\FVFileReader.dof
 
 echo   FVReport
 copy Reporter\FVReport.cfg.tplt Reporter\FVReport.cfg
-copy Reporter\FVReport.dof.tplt Reporter\FVReport.dof
 
 echo DONE
 
