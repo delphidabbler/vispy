1101d546c683a185f7d38b59d33dbeef0ee154ab
DelphiDabbler <git@delphidabbler.com>
2011-03-28 16:56:33 +0000

    vis: * Changed to use ZIPROOT environment variable, if set, to reference folder containing ZIP.exe.

--- DevTools/Release.bat
+++ DevTools/Release.bat
@@ -2,7 +2,10 @@
 @rem Script used to create zip file containing binary release of Version
 @rem Information Spy
 @rem
-@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2007-2009
+@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2007-2011
+@rem
+@rem Uses the ZIPROOT environment variable, if set, to find Zip.exe. If ZIPROOT
+@rem is not set Zip.exe must be on the system path.
 @rem
 @rem $Rev$
 @rem $Date$
@@ -11,6 +14,9 @@
 @echo off
 
 setlocal
+
+set ZIPEXE="%ZIPROOT%\Zip.exe"
+if "%ZIPROOT%" == "" set ZIPEXE="Zip.exe"
 
 cd ..
 
@@ -21,7 +27,7 @@ if not exist Release mkDir Release
 if exist %OutFile% del %OutFile%
 
 rem Create zip file containing setup program and readme
-zip -j -9 %OutFile% Exe\VIS-Setup-*.exe
-zip -j -9 %OutFile% Docs\ReadMe.htm
+%ZIPEXE% -j -9 %OutFile% Exe\VIS-Setup-*.exe
+%ZIPEXE% -j -9 %OutFile% Docs\ReadMe.htm
 
 endlocal
